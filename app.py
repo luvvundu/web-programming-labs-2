5a13f39e85c82429430cc097019eb0fca31ce410
Victoria Itegelova <vv8834480@gmail.com>
2023-09-23 20:46:51 +0700

    Статистические файлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -55,4 +55,17 @@ def lab1():
 </html> 
 """
 
-
+@app.route('/lab1/oak')
+def oak():
+    return '''
+<!doctype html>
+    <head>
+    <link rel="stylesheet" type="text/css" href=''' + url_for('static', filename='lab1.css') + ''' />
+    </head>
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src='''+ url_for('static', filename='oak.jpg') + '''>
+    </body>
+</html>
+'''
