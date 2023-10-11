f1927b5933a801e2266632e02929d91a54ace144
Victoria Itegelova <vv8834480@gmail.com>
2023-10-11 12:52:23 +0700

    Лабораторная работа 3 Восстановление файлов

---
+++ lab3.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, request
+lab3 = Blueprint('lab3', __name__)
+
+
+@lab3.route('/lab3/')
+def lab():
+    return render_template ('lab3.html')
+
+@lab3.route('/lab3/form1')
+def form1():
+    errors = {}
+    user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+    sex = request.args.get('sex')
+    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
