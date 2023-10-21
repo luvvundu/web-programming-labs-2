f2827a0cf6a585c8ded0b5aa94d855a939b446ee
Victoria Itegelova <vv8834480@gmail.com>
2023-10-21 16:11:46 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -5,3 +5,29 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods = ['GET','POST'])
+def login():
+    error_username = None
+    error_password = None
+
+    if request.method == 'GET': 
+        return render_template('login.html')
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+        return render_template('success.html')
+    
+    if not username:
+        error_username = "Не введен логин"
+
+    if not password:
+        error_password = "Не введен пароль"
+    
+    error = 'Неверные логин и/или пароль'
+    return render_template('login.html', error=error, error_username=error_username, error_password=error_password)
+
+    if __name__ == "__main__":
+        app.run()
