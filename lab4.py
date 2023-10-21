97a19bda74191745254e139ee9de8234b05af035
Victoria Itegelova <vv8834480@gmail.com>
2023-10-21 16:35:49 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -29,5 +29,24 @@ def login():
     error = 'Неверные логин и/или пароль'
     return render_template('login.html', error=error, error_username=error_username, error_password=error_password)
 
-    if __name__ == "__main__":
-        app.run()
+
+@lab4.route("/lab4/fridge", methods=["GET", "POST"])
+def fridge():
+    if request.method == "POST":
+        temperature = request.form.get("temperature")
+
+        if not temperature:
+            error = "Ошибка: не задана температура"
+            return render_template("fridge.html", error=error)
+
+        temperature = int(temperature)
+        if temperature < -12:
+            error = "Не удалось установить температуру — слишком низкое значение"
+        elif temperature > -1:
+            error = "Не удалось установить температуру — слишком высокое значение"
+        else:
+            error = None
+
+        return render_template("fridge.html", temperature=temperature, error=error)
+
+    return render_template("fridge.html")
