de93bc10ca8bd4487ecc67644967477381f787b9
Victoria Itegelova <vv8834480@gmail.com>
2023-12-09 16:49:49 +0700

    Редактирование курсов

--- static/lab8/main.js
+++ static/lab8/main.js
@@ -18,6 +18,9 @@ function fillCourseList() {
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
+            editButton.onclick = function() {
+                editCourse(i, courses[i]);
+            };
 
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
@@ -42,9 +45,58 @@ function fillCourseList() {
 function deleteCourse(num) {
     if(! confirm('Вы точно хотите удалить курс?'))
         return;
-    
-    fetch('/lab8/api/courses/${num}', {method: 'DELETE'})
+
+    fetch(`/lab8/api/courses/${num}`, {method: 'DELETE'})
     .then(function () {
         fillCourseList();
     });
+}
+
+function showModal() {
+    document.querySelector('div.modal').style.display = 'block';
+}
+
+function hideModal() {
+    document.querySelector('div.modal').style.display = 'none';
+}
+
+function cancel() {
+    hideModal();
+}
+
+function addCourse() {
+    document.getElementById('num').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('videos').value = '';
+    document.getElementById('price').value = '';
+    showModal();
+}
+
+function sendCourse() {
+    const num = document.getElementById('num').value;
+    const course = {
+        name: document.getElementById('name').value,
+        videos: document.getElementById('videos').value,
+        price: document.getElementById('price').value
+    }
+
+    const url = `/lab8/api/courses/${num}`;
+    const method = num ? 'PUT' : 'POST';
+    fetch(url, {
+        method: method,
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(course)
+    })
+    .then(function() {
+        fillCourseList();
+        hideModal();
+    });
+}
+
+function editCourse(num, course) {
+    document.getElementById('num').value - num;
+    document.getElementById('name').value = course.name;
+    document.getElementById('videos').value = course.videos;
+    document.getElementById('price').value = course.price;
+    showModal();
 }
